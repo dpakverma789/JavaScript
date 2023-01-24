e7219d8cb591ac99a4624affa928004d26c71063
deepak_verma <deepak.verma@logicloop.io>
2023-01-24 14:51:48 +0530

    exception handling

--- error_handling.js
+++ error_handling.js
@@ -1,8 +1,18 @@
 
 try{
-	console.log('hello deepak');
+	let status = 200;
+	if (status == 200){
+		console.log('hello deepak');
+	}
+	else{
+		throw "status code is not 200";
+	}
 }
 
 catch(err){
-	console.log('something went wrong');
+	console.log(err);
+}
+
+finally{
+	console.log('Exceptional Handling complete!')
 }
